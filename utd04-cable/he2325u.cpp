9727de08d4b65873fc79bbbf40a7457032e60faf
Jaroslav Sykora <jaroslav.sykora@gmail.com>
2014-06-05 22:27:55 +0200

    Cable software, VC870 decoder

--- utd04-cable/he2325u.cpp
+++ utd04-cable/he2325u.cpp
@@ -41,119 +41,148 @@
 
 // Headers needed for sleeping.
 #ifdef _WIN32
-	#include <windows.h>
+    #include <windows.h>
 #else
-	#include <unistd.h>
+    #include <unistd.h>
 #endif
 
 int main(int argc, char* argv[])
 {
-	int res;
-	unsigned char buf[256];
-	#define MAX_STR 255
-	hid_device *handle=0;
-	int i;
-        int dev_cnt = 0;
+    int res;
+    unsigned char buf[256];
+    #define MAX_STR 255
+    hid_device *handle=0;
+    int i;
+    int dev_cnt = 0;
 
 #ifdef WIN32
-	UNREFERENCED_PARAMETER(argc);
-	UNREFERENCED_PARAMETER(argv);
+    UNREFERENCED_PARAMETER(argc);
+    UNREFERENCED_PARAMETER(argv);
 #endif
 
-	struct hid_device_info *devs, *cur_dev;
-	
+    struct hid_device_info *devs, *cur_dev;
+    int bps = -1;
 
-        if(argc==1) // if no params are supplied list available devs
-        {
-            devs = hid_enumerate(0x1a86, 0xe008); // all chips this SW belongs to...
-	    cur_dev = devs;	
-            while(cur_dev){cur_dev = cur_dev->next; dev_cnt++; }
-            printf("[!] found %i devices:\n",dev_cnt);
+    int optch;
 
-            cur_dev = devs;	
-	    while (cur_dev) {
-		printf("%s\n", cur_dev->path);
-		cur_dev = cur_dev->next;
-	    }
+    while ((optch = getopt(argc, argv, "b:h")) > 0) {
+        switch (optch) {
+            case 'b': {
+                // set baud speed
+                if (sscanf(optarg, "%d", &bps) != 1) {
+                    fprintf(stderr, "Baud rate after -b must be a number. Given: %s\n", optarg);
+                    exit(1);
+                }
+                break;
+            }
+            case 'h': {
+                // print help
+                printf("Usage help:\n");
+                printf("  utd04-cable -b <baudrate> [<usbdevice>]\n");
+                printf("    <baudrate>:  19200 for UNI-T UT61E\n");
+                printf("                  9600 for VOLTCRAFT VC-870\n");
+                exit(0);
+            }
+        }
+    }
+    
 
-            hid_free_enumeration(devs);
+    if(argc == optind) // if no params are supplied list available devs
+    {
+        devs = hid_enumerate(0x1a86, 0xe008); // all chips this SW belongs to...
+        cur_dev = devs; 
+        while(cur_dev){cur_dev = cur_dev->next; dev_cnt++; }
+        printf("[!] found %i devices:\n",dev_cnt);
+
+        cur_dev = devs; 
+        while (cur_dev) {
+           printf("%s\n", cur_dev->path);
+           cur_dev = cur_dev->next;
         }
 
+        hid_free_enumeration(devs);
+    }
 
-        if(argc>1) // use the supplied device (if any)
-        {
-            handle = hid_open_path(argv[1]);
-        }	
 
-	// Open the device using the VID, PID,
-	// and optionally the Serial number (NULL for the hoitek chip).
-        if(handle==0) handle = hid_open(0x1a86, 0xe008, NULL); // 1a86 e008
+    if (optind < argc) // use the supplied device (if any)
+    {
+        handle = hid_open_path(argv[optind]);
+    }   
 
-	if (!handle) {
-		printf("unable to open device\n");
- 		return 1;
-	}
+    // Open the device using the VID, PID,
+    // and optionally the Serial number (NULL for the hoitek chip).
+    if (handle==0) {
+        handle = hid_open(0x1a86, 0xe008, NULL); // 1a86 e008
+    }
+
+    if (!handle) {
+        fprintf(stderr, "unable to open device\n");
+        return 1;
+    }
 
 
 /*
-	// Read the Product String
-	wstr[0] = 0x0000;
-	res = hid_get_product_string(handle, wstr, MAX_STR);
-	if (res < 0)
-		printf("Unable to read product string\n");
-	printf("Product String: %ls\n", wstr);
+    // Read the Product String
+    wstr[0] = 0x0000;
+    res = hid_get_product_string(handle, wstr, MAX_STR);
+    if (res < 0)
+        printf("Unable to read product string\n");
+    printf("Product String: %ls\n", wstr);
 */
 
-	// Set the hid_read() function to be non-blocking.
-//	hid_set_nonblocking(handle, 1);
+    // Set the hid_read() function to be non-blocking.
+//  hid_set_nonblocking(handle, 1);
 
 
 
-	memset(buf,0x00,sizeof(buf));
-	
-        //unsigned int bps = 19200;
-        unsigned int bps = 9600;
-	// Send a Feature Report to the device
-	buf[0] = 0x0; // report ID
-        buf[1] = bps;
-        buf[2] = bps>>8;
-        buf[3] = bps>>16;
-        buf[4] = bps>>24;
-	buf[5] = 0x03; // 3 = enable?
-	res = hid_send_feature_report(handle, buf, 6); // 6 bytes
-	if (res < 0) {
-		printf("Unable to send a feature report.\n");
-	}
+    memset(buf,0x00,sizeof(buf));
+    
+    //unsigned int bps = 19200;
+    // unsigned int bps = 9600;
+    if (bps < 0) {
+        fprintf(stderr, "Baudspeed not specified! Use -b <baudspeed> command line option.\n");
+        exit(1);
+    }
+    // Send a Feature Report to the device
+    buf[0] = 0x0; // report ID
+    buf[1] = bps;
+    buf[2] = bps>>8;
+    buf[3] = bps>>16;
+    buf[4] = bps>>24;
+    buf[5] = 0x03; // 3 = enable?
+    res = hid_send_feature_report(handle, buf, 6); // 6 bytes
+    if (res < 0) {
+        fprintf(stderr, "Unable to send a feature report.\n");
+    }
 
-	memset(buf,0,sizeof(buf));
+    memset(buf,0,sizeof(buf));
 
-        printf("-data start-\n");
+    printf("-data start-\n");
 
-        usleep(1000);
+    usleep(1000);
 
-        do
+    do {
+
+        res = 0;
+        while (res == 0)
         {
+            res = hid_read(handle, buf, sizeof(buf));
+            if (res == 0)
+                fprintf(stderr, "waiting...\n");
+            if (res < 0)
+                fprintf(stderr, "Unable to read()\n");
+        }
 
-            res = 0;
-	    while (res == 0)
-                {
-		    res = hid_read(handle, buf, sizeof(buf));
-		    if (res == 0)
-			printf("waiting...\n");
-		    if (res < 0)
-			printf("Unable to read()\n");
-	        }
+        // format data 
+        int len=buf[0] & 7; // the first byte contains the length in the lower 3 bits ( 111 = 7 )
+        for (i=0; i<len; i++)
+            buf[i+1] &= 0x7f; // bitwise and with 0111 1111, mask the upper bit which is always 1
 
-            // format data 
-            int len=buf[0] & 7; // the first byte contains the length in the lower 3 bits ( 111 = 7 )
-            for (i=0; i<len; i++)
-                buf[i+1] &= 0x7f; // bitwise and with 0111 1111, mask the upper bit which is always 1
-
-            if(len>0)
-            {
-                fwrite(buf+1, 1, len, stdout); // write data directly to stdout to enable pipeing to interpreter app
-                fflush(stdout);
-            }
+        if(len>0)
+        {
+            fwrite(buf+1, 1, len, stdout); // write data directly to stdout to enable pipeing to interpreter app
+            fflush(stdout);
+        }
 
 
         }while(res>=0);
@@ -161,8 +190,8 @@ int main(int argc, char* argv[])
         hid_close(handle);
 
 #ifdef WIN32
-	system("pause");
+    system("pause");
 #endif
 
-	return 0;
+    return 0;
 }
---
+++ vc870-decode/vc870_decode.pl
@@ -0,0 +1,266 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+# use Switch;
+
+my %fun_sel = (
+    '00' => 'DCV',
+    '01' => 'ACV',
+    '10' => 'DCmV',
+    '11' => 'TEMP',
+    '20' => 'RES',
+    '21' => 'CTN',
+    '30' => 'CAP',
+    '40' => 'DIO',
+    '50' => 'FREQ',
+    '51' => '(4~20)mA',
+    '60' => 'DCuA',
+    '61' => 'ACuA',
+    '70' => 'DCmA',
+    '71' => 'ACmA',
+    '80' => 'DCA',
+    '81' => 'ACA',
+    '90' => 'Act+Apar_Power',
+    '91' => 'PowFactor+Freq',
+    '92' => 'VoltEff+CurrEff'
+);
+
+while (<STDIN>) {
+    my @ivals = /^(\d{2})(\d)(\d{5})(\d{5})(\d{2})(\d)(\d)(\d)(\d)(\d)(\d)/;
+    if (not defined($ivals[0])) {
+        next;
+    }
+
+    # print(join(' ', @ivals) . "\n");
+
+    my $fun = $fun_sel{$ivals[0]};
+    print($fun . "  ");
+
+    my ($div, $unit, $unit2, $div2);
+
+    if (($fun eq 'DCV') or ($fun eq 'ACV')) {
+        $unit = 'V';
+        if ($ivals[1] eq '0') {
+            # 4V
+            $div = 10000;
+        } elsif ($ivals[1] eq '1') {
+            # 40V
+            $div = 1000;
+        } elsif ($ivals[1] eq '2') {
+            # 400V
+            $div = 100;
+        } elsif ($ivals[1] eq '3') {
+            # 1000V
+            $div = 10;
+        }
+    } elsif ($fun eq 'DCmV') {
+        $unit = 'mV';
+        $div = 100;
+    } elsif ($fun eq 'RES') {
+        if ($ivals[1] eq '0') {
+            # 400 ohm
+            $div = 100;
+            $unit = 'Ohm';
+        } elsif ($ivals[1] eq '1') {
+            # 4 kOhm
+            $div = 1000;
+            $unit = 'kOhm';
+        } elsif ($ivals[1] eq '2') {
+            # 40 kOhm
+            $div = 100;
+            $unit = 'kOhm';
+        } elsif ($ivals[1] eq '3') {
+            # 400 kOhm
+            $div = 10;
+            $unit = 'kOhm';
+        } elsif ($ivals[1] eq '4') {
+            # 4 MOhm
+            $div = 1000;
+            $unit = 'MOhm';
+        } elsif ($ivals[1] eq '5') {
+            # 40 MOhm
+            $div = 10000;
+            $unit = 'MOhm';
+        }
+    } elsif ($fun eq 'CAP') {
+        if ($ivals[1] eq '0') {
+            # 40 nF
+            $div = 1000;
+            $unit = 'nF';
+        } elsif ($ivals[1] eq '1') {
+            # 400 nF
+            $div = 100;
+            $unit = 'nF';
+        } elsif ($ivals[1] eq '2') {
+            # 4000 nF
+            $div = 10;
+            $unit = 'nF';
+        } elsif ($ivals[1] eq '3') {
+            # 40 uF
+            $div = 1000;
+            $unit = 'uF';
+        } elsif ($ivals[1] eq '4') {
+            # 400 uF
+            $div = 100;
+            $unit = 'uF';
+        } elsif ($ivals[1] eq '5') {
+            # 4 mF
+            $div = 10000;
+            $unit = 'mF';
+        } elsif ($ivals[1] eq '6') {
+            # 40 mF
+            $div = 1000;
+            $unit = 'mF';
+        }
+    } elsif (($fun eq 'DCuA') or ($fun eq 'ACuA')) {
+        if ($ivals[1] eq '0') {
+            # 400 uA
+            $div = 100;
+            $unit = 'uA';
+        } elsif ($ivals[1] eq '1') {
+            # 4000 uA
+            $div = 10;
+            $unit = 'uA';
+        }
+    } elsif (($fun eq 'DCmA') or ($fun eq 'ACmA')) {
+        if ($ivals[1] eq '0') {
+            # 40 mA
+            $div = 1000;
+            $unit = 'mA';
+        } elsif ($ivals[1] eq '1') {
+            # 400 mA
+            $div = 100;
+            $unit = 'mA';
+        }
+    } elsif (($fun eq 'DCA') or ($fun eq 'ACA')) {
+        $div = 1000;
+        $unit = 'A';
+    } elsif ($fun eq 'TEMP') {
+        $div = 10;
+        $unit = 'Celsius';
+        $unit2 = 'Farenheit';
+    } elsif ($fun eq 'CTN') {
+        $div = 100;
+        $unit = 'Ohm';
+    } elsif ($fun eq 'DIO') {
+        $div = 10000;
+        $unit = 'V';
+    } elsif ($fun eq 'FREQ') {
+        $div = 1;
+        $unit = 'Hz';
+    } elsif ($fun eq '(4~20)mA') {
+        $div = 1;
+        $unit = '%';
+    } elsif ($fun eq 'Act+Apar_Power') {
+        $div = 10;
+        $unit = 'W';
+        $unit2 = 'VA';
+    } elsif ($fun eq 'PowFactor+Freq') {
+        $div = 1000;
+        $unit = 'cos_fi';
+        $div2 = 10;
+        $unit2 = 'Hz';
+    } elsif ($fun eq 'VoltEff+CurrEff') {
+        $div = 10;
+        $unit = 'V';
+        $unit2 = 'A';
+    }
+
+
+    if (not defined($unit2)) {
+        $unit2 = $unit;
+    }
+    if (not defined($div2)) {
+        $div2 = $div;
+    }
+
+    my $status = $ivals[5];
+    my $option1 = $ivals[6];
+    my $option2 = $ivals[7];
+    my $option3 = $ivals[8];
+    my $option4 = $ivals[9];
+    my $option5 = $ivals[10];
+
+    my $sign2_flag = ($status & 0x08);
+    my $sign1_flag = ($status & 0x04);
+    my $batt_flag = ($status & 0x02);
+    my $ol1_flag = ($status & 0x01);
+
+    my $max_flag = ($option1 & 0x08);
+    my $min_flag = ($option1 & 0x04);
+    my $maxmin_flag = ($option1 & 0x02);
+    my $rel_flag = ($option1 & 0x01);
+
+    my $ol2_flag = ($option2 & 0x08);
+    my $open_flag = ($option2 & 0x04);
+    my $manu_flag = ($option2 & 0x02);
+    my $hold_flag = ($option2 & 0x01);
+
+    my $light_flag = ($option3 & 0x08);
+    my $usb_flag = ($option3 & 0x04);
+    my $warn_flag = ($option3 & 0x02);
+    my $auto_power_flag = ($option3 & 0x01);
+
+    my $misplug_flag = ($option4 & 0x08);
+    my $lo_flag = ($option4 & 0x04);
+    my $hi_flag = ($option4 & 0x02);
+    my $open2_flag = ($option4 & 0x01);
+
+    my $dualdisp_flag = ($option5 & 0x01);
+
+    my $v1 = $ivals[2] / $div;
+    if ($sign1_flag > 0) {
+        print("-");
+    }
+    if (($ol1_flag > 0) or ($open_flag > 0)) {
+        $v1 = '-OL-';
+    }
+    print("$v1 $unit");
+
+    if ($max_flag > 0) {
+        print(" MAX");
+    }
+    if ($min_flag > 0) {
+        print(" MIN");
+    }
+    if ($maxmin_flag > 0) {
+        print(" MAXMIN");
+    }
+    if ($rel_flag > 0) {
+        print(" REL");
+    }
+
+    if ($dualdisp_flag > 0) {
+        my $v2 = $ivals[3] / $div2;
+        if (($ol2_flag > 0) or ($open_flag > 0)) {
+            $v2 = '-OL-';
+        }
+        if ($sign2_flag > 0) {
+            print("-");
+        }
+        print("  $v2 $unit2");
+    }
+
+    if ($open_flag > 0) {
+        print(" OPEN");
+    }
+    if ($manu_flag > 0) {
+        print(" MANU");
+    }
+    if ($hold_flag > 0) {
+        print(" HOLD");
+    }
+
+    if ($lo_flag > 0) {
+        print(" LO");
+    }
+    if ($hi_flag > 0) {
+        print(" HI");
+    }
+    if ($open2_flag > 0) {
+        print(" OPEN2");
+    }
+
+    print("\n");
+}
